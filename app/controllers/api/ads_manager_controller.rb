aa3597c8eac547ae38e696c1d9a379cf1acba91d
mojo1643 <johnrejimathai@gmail.com>
2015-05-23 21:55:25 +0530

    api_testing

--- app/controllers/api/ads_manager_controller.rb
+++ app/controllers/api/ads_manager_controller.rb
@@ -2,10 +2,10 @@ class API::AdsManagerController < ApplicationController
 
   def index
     # ads = AdsManager.all
-    lat = params[:lat]
-    lon = params[:lon]
+    lat = params[:lat].to_i
+    lon = params[:lon].to_i
     
-    adlocation = AdsManager.where("lat between '13.011111' and '13.444444' and lon between '80.000001' and '80.9999999'")
+    adlocation = AdsManager.where("lat between '#{lat+0.011111}' and '#{lat+0.444444}' and lon between '#{lon+0.000001}' and '#{lon+0.9999999}'")
     adlocation = {'adlocation' => adlocation}
     respond_to do |format|
       format.json { render :json => adlocation, status: 201 }
