b6fef8a9a9eaf5c99c10bfdfb42c69c4b436a0a0
mojo1643 <johnrejimathai@gmail.com>
2015-06-02 21:13:37 +0530

    json data

--- app/controllers/api/v1/registrations_controller.rb
+++ app/controllers/api/v1/registrations_controller.rb
@@ -3,8 +3,7 @@ class API::V1::RegistrationsController < Devise::RegistrationsController
   def create
     @user = User.create(user_params)
     if @user.save
-      user = {:state => {:code => 0}, :data => @user }
-      render :json => {:user => user}
+      render :json => {:state => {:code => 0}, :data => @user }
     else
       render :json => {:state => {:code => 1, :messages => @user.errors.full_messages} }
     end
