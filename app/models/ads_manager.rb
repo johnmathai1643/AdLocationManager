a6d731ef10574cddcfd257648367e57081897b24
mojo1643 <johnrejimathai@gmail.com>
2015-07-26 13:22:25 +0530

    figaro changes

--- app/models/ads_manager.rb
+++ app/models/ads_manager.rb
@@ -5,7 +5,7 @@ class AdsManager < ActiveRecord::Base
      after_validation :geocode, :reverse_geocode
 
 
-  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png", :url  => ":s3_domain_url", :path => "public/avatars/:id/:style_:basename.:extension", :storage => :fog, :fog_credentials => {provider: 'AWS', aws_access_key_id: 'AKIAJV42PGGRCX7MUIGA', aws_secret_access_key: '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', :region => 'us-west-2'}, fog_directory: 'adlocation-dev'
+  has_attached_file :avatar, :styles => { :medium => "300x300>", :thumb => "100x100#" }, :default_url => "/images/:style/missing.png", :url  => ":s3_domain_url", :path => "public/avatars/:id/:style_:basename.:extension", :storage => :fog, :fog_credentials => {provider: 'AWS', aws_access_key_id: ENV["AWS_ACCESS_KEY_ID"], aws_secret_access_key: ENV["AWS_SECRET_ACCESS_KEY"], :region => 'us-west-2'}, fog_directory: ENV[" FOG_DIRECTORY"]
 
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
